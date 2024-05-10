0454f14ec9676a4d01ded28481b4fd5960b99f23
christian <christianbiringanine22@gmail.com>
2024-05-10 04:06:56 +0200

    Implement the top cards comp

--- src/components/utils/card.jsx
+++ src/components/utils/card.jsx
@@ -1,9 +1,9 @@
-const TopCard = () => {
+const TopCard = ({ title, number, color}) => {
   return (
-    <div className="card-container">
-      <p className="title">Total Sales</p>
+    <div className="bg-cyan-700 card-content">
+      <p className="title">{title}</p>
       <div className="details">
-        <p className="number">$59,069</p>
+        <p className="number">$ {number}</p>
         <p>graph</p>
       </div>
       <p>Since last week <span className="growth">+13.4%</span></p>
